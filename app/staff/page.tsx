aab2e4e3c519ab8bcc91d342185b59b29f65ab92
dhekmass12 <dimas.tri01@ui.ac.id>
2024-05-27 00:09:31 +0700

    fix typo

---
+++ app/staff/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import Header from '@/components/Header';
+import ListingList, { Listing } from '@/components/staff/ListingList';
+import { GetServerSideProps } from 'next';
+import { useVerifyMutation } from '@/redux/features/authApiSlice';
+import { useGetProfileQuery } from '@/redux/features/authApiSlice';
+
+
+export default function Page() {
+    const [listings, setListings] = useState<Listing[] >();
+    const [verify, isLoading] = useVerifyMutation();
+
+    //const { data: user, isLoading, isFetching, refetch } = useGetProfileQuery();
+
+    const fetchAllListings = async () => {
+      const response = await fetch(`http://34.87.41.75/staff/reported-listing`)
+      const data = await response.json();
+      setListings(data);
+    };
+
+    useEffect(() => {
+      verify(undefined)
+			.unwrap()
+			.then((response) => {
+              fetchAllListings();
+			});
+    }, []);
+  
+    return (
+      <div>
+        {listings && <ListingList listings={listings}/>}
+      </div>
+    );
+}
