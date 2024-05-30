ab770076739fc7e173cc9bc7aa67d8d9e8a17e41
dhekmass12 <dimas.tri01@ui.ac.id>
2024-05-31 04:19:17 +0700

    fix report listing feature

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -7,14 +7,14 @@ export default function Page() {
     return (
         <div className="listing-list">
             <div className="flex justify-center my-8">
-                <a href="/catalogue/create" className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">
-                    Add New Listing
+                <a href="/staff/reported-listings" className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">
+                    Reported Listings
                 </a>
             </div>
 
             <div className="flex justify-center my-8">
-                <a href="/catalogue/order" className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">
-                    Orders Management
+                <a href="/staff/top-ups" className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">
+                    Top Up Transactions
                 </a>
             </div>
         </div>
